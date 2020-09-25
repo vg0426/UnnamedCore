d9bf94e6dc7f18feecc71e9520cc0ce1375a4a43
vg0426 <vg0426@gmail.com>
2020-09-25 16:26:40 -0400

    first commit

---
+++ src/Providers/UnnamedCoreServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace VGlance\UnnamedCore\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UnnamedCoreServiceProvider extends ServiceProvider
+{
+    public function boot() {
+        
+    }
+
+    public function register() {
+
+    }
+}
